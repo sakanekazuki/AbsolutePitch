4b27fabf2625862733a362041038953e74fdc23c
sakanekazuki <kazuki825@outlook.jp>
2023-04-11 23:28:18 +0900

    add chord

--- Assets/00_sakane/script/common/AudioTypeManager.cs
+++ Assets/00_sakane/script/common/AudioTypeManager.cs
@@ -9,33 +9,47 @@ public class AudioTypeManager
 	// SoundType
 	public enum SoundType
 	{
-		C			= 0,  // ド
-		CSharp		= 1,  // ド＃
-		DFlat		= 2,  // レ♭
-		D			= 3,  // レ
-		DSharp		= 4,  // レ＃
-		EFlat		= 5,  // ミ♭
-		E			= 6,  // ミ
-		ESharp		= 7,  // ミ＃（ファ）
-		F			= 8,  // ファ
-		FSharp		= 9,  // ファ＃
-		GFlat		= 10, // ソ♭
-		G			= 11, // ソ
-		GSharp		= 12, // ソ＃
-		AFlat		= 13, // ラ♭
-		A			= 14, // ラ
-		ASharp		= 15, // ラ＃
-		BFlat		= 16, // シ♭
-		B			= 17, // シ
-		BSharp		= 18, // シ＃
-		CCode		= 19, // Cコード	（ド、		ミ、	ソ）
-		CSharpCode	= 20, // C＃コード	（ド＃、	ミ＃、	ソ＃）
-		DFlatCode	= 21, // C♭コード	（レ♭、	ファ、	ラ♭）
-		DCode		= 22, // Dコード	（レ、		ファ＃、ラ）
-		DSharpCode	= 23, // D＃コード	（レ＃、	ソ、	ラ＃）
-		EFlatCode	= 24, // E♭コード	（ミ♭、	ソ、	シ♭）
-		ECode		= 25, // Eコード	（ミ、		ソ＃、	シ）
-		FCode		= 26, // Fコード	（ファ、	ラ、	ド）
-		FSharpCode	= 27, // F＃コード	（ファ＃、	ラ＃、	ド＃）
+		C		= 0,  // ド
+		CSharp	= 1,  // ド＃
+		DFlat	= 2,  // レ♭
+		D		= 3,  // レ
+		DSharp	= 4,  // レ＃
+		EFlat	= 5,  // ミ♭
+		E		= 6,  // ミ
+		ESharp	= 7,  // ミ＃（ファ）
+		F		= 8,  // ファ
+		FSharp	= 9,  // ファ＃
+		GFlat	= 10, // ソ♭
+		G		= 11, // ソ
+		GSharp	= 12, // ソ＃
+		AFlat	= 13, // ラ♭
+		A		= 14, // ラ
+		ASharp	= 15, // ラ＃
+		BFlat	= 16, // シ♭
+		B		= 17, // シ
+		BSharp	= 18, // シ＃
+	}
+
+	// MajorChord
+	public enum MajorCodeType
+	{
+		CChord		= 0,  // Cコード	（ド、		ミ、	ソ）
+		CSharpChord = 1,  // C＃コード	（ド＃、	ミ＃、	ソ＃）
+		DFlatChord	= 2,  // C♭コード	（レ♭、	ファ、	ラ♭）
+		DChord		= 3,  // Dコード	（レ、		ファ＃、ラ）
+		DSharpChord = 4,  // D＃コード	（レ＃、	ソ、	ラ＃）
+		EFlatChord	= 5,  // E♭コード	（ミ♭、	ソ、	シ♭）
+		EChord		= 6,  // Eコード	（ミ、		ソ＃、	シ）
+		FChord		= 7,  // Fコード	（ファ、	ラ、	ド）
+		FSharpChord = 8,  // F＃コード	（ファ＃、	ラ＃、	ド＃）
+		GFlatChord	= 9,  // G♭コード	（ソ♭、	シ♭、	レ♭）
+		GChord		= 10, // Gコード	（ソ、		シ、	レ）
+		GSharpShord = 11, // G＃コード	（ソ＃、	シ＃、	レ＃）
+		AFlatShord	= 12, // A♭コード	（ラ♭、	ド、	ミ♭）
+		AShord		= 13, // Aコード	（ラ、		ド＃、	ミ）
+		ASharpShord	= 14, // A＃コード	（ラ＃、	レ、	ファ）
+		BFlatShord	= 15, // B♭コード	（シ♭、	レ、	ファ）
+		BShord		= 16, // Bコード	（シ、		レ＃、	ファ＃）
+
 	}
 }
